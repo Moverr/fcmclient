d1281d3d91dd76c2fc54fe9ddef57deb2aa9d741
Muyinda ROgers <moverr@gmail.com>
2020-05-23 15:44:18 +0300

    :Debugggigng

--- app/src/main/java/com/kodeinc/fmclient/FirebaseMessagingService.java
+++ app/src/main/java/com/kodeinc/fmclient/FirebaseMessagingService.java
@@ -4,20 +4,49 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    public FirebaseMessagingService() {
+        super();
+    }
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         showNotification(remoteMessage.getData().get("message"));
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+
+        try{
+          String mToken =   FirebaseInstanceId.getInstance().getToken(s,s);
+            Log.e("TOKEN",mToken);
+            Toast.makeText(this, mToken, Toast.LENGTH_LONG).show();
+
+        }catch (IOException e){
+
+        }
+
+
+
+    }
+
+    @Override
+    public void onMessageSent(@NonNull String s) {
+        super.onMessageSent(s);
     }
 
     public void showNotification(String Message){
@@ -42,6 +71,8 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
                 .setContentIntent(pendingIntent);
 
 
-        NotificationManager notificationManager
+        NotificationManager manager =(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(0,builder.build());
+
     }
 }
