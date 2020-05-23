9612488cfc3499652de06b9b2ce24333d266c228
Muyinda ROgers <moverr@gmail.com>
2020-05-23 14:20:55 +0300

    :Debugging

--- app/src/main/java/com/kodeinc/fmclient/FirebaseMessagingService.java
+++ app/src/main/java/com/kodeinc/fmclient/FirebaseMessagingService.java
@@ -1,6 +1,11 @@
 package com.kodeinc.fmclient;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -15,6 +20,24 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
     }
 
     public void showNotification(String Message){
+        Intent i = new Intent(this,MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        Notification noti = new Notification.Builder(this)
+                .setContentTitle("Notificaton")
+                .setContentText(Message)
+                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        NotificationCompat.Builder builder =  new NotificationCompat.Builder(this,null)
+                .setAutoCancel(true)
+                .setContentTitle("Testing Me MOver")
+                .setContentText(Message)
+                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+                .setContentIntent(pendingIntent);
     }
 }
